6508563cb9432118057f974540e3ead20a319386
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-14 16:13:34 -0700

    adding remember me flag, setting more cookies, redirect if logged in

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -8,10 +8,10 @@ class LoginController extends ApplicationController {
     }
 
     public function index(){
-
-//        $this->rc->results = $this->mysql->setQuery("select * from roster")->runQuery();
-
-        $this->rc->test = 'login controller' ;
+        if($this->isLoggedIn){
+            header('Location: /');
+        }
+        $this->rc->time = time();
         $this->view = 'login/index';
         return $this;
     }
@@ -24,15 +24,31 @@ class LoginController extends ApplicationController {
         // query
         $email = $this->router->getVar('email');
         $password = $this->router->getVar('password');
-        $this->rc->results = $this->mysql->setQuery("select id, isAdmin, email from roster where email = '$email' and password = '$password'")->runQuery();
+        $this->rc->results = $this->mysql->setQuery("select id, isAdmin, email from roster where email = '$email' and password = '$password' and isActive = 1")->runQuery();
+
+        // remember me
+        $__rememberMe = $this->router->getVar('rememberMe');
 
         $this->isLoggedIn = count($this->rc->results) > 0;
         if($this->isLoggedIn){
-            setcookie('isLoggedIn', 'true', time() + 3600, '/');
-            setcookie('isAdmin', $this->rc->results[0]->isAdmin, time() + 3600, '/');
-            setcookie('email', $this->rc->results[0]->email, time() + 3600, '/');
+
+            // 1 day vs 1 year =)
+            $cookieDuration = $__rememberMe == 1
+                ? (60 * 60 * 24 * 30 * 3) // an hour
+                : (60 * 60); // approx 3 months
+            //$cookieDuration = 60 * 60;
+
+            setcookie('isLoggedIn', 'true', time() + $cookieDuration, '/');
+            setcookie('id', $this->rc->results[0]->id, time() + $cookieDuration, '/');
+            setcookie('isAdmin', $this->rc->results[0]->isAdmin, time() + $cookieDuration, '/');
+            setcookie('email', $this->rc->results[0]->email, time() + $cookieDuration, '/');
+            setcookie('data', serialize($this->rc->results[0]), time() + $cookieDuration, '/');
+            if($__rememberMe == 1){
+                setcookie('rememberMe', '1', time()+$cookieDuration, '/');
+            }
+
             // TODO: hash the password and stuff
-//            setcookie('token', hash('sha1', $this->rc->results[0]->password));
+            // setcookie('token', hash('sha1', $this->rc->results[0]->password));
         }
 
         // handle the login
