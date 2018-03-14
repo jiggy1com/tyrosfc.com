e313b59185c1c25d4fbe2ebd74eb3fd7dc8ee0e8
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-14 16:15:37 -0700

    refactoring, remove unused code, add redirect

--- controller/ScheduleController.php
+++ controller/ScheduleController.php
@@ -1,89 +1,115 @@
 <?php
 
-class ScheduleController extends ApplicationController {
+class ScheduleController extends ApplicationController
+{
 
     function __construct()
     {
         parent::__construct();
     }
 
-    public function index(){
-        $this->rc->test = true;
-        if( !$this->isLoggedIn ){
+    public function index()
+    {
+        if (!$this->isLoggedIn) {
+            $this->setRedirectToSelf();
             return $this->setNextRoute('/login');
-        }else{
+        } else {
+
+            $s = new Schedule();
+            $this->rc->schedule = $s->getSchedule();
+
+            $query = "select * from attendance where rosterId = " . $this->cookies['id'];
+            $this->rc->attendance = $this->mysql->setQuery($query)->runRead();
+
             $this->view = 'schedule/index';
             return $this;
         }
     }
 
-    public function test(){
-        $this->rc->test = true;
-        $this->view = 'schedule/test';
-        $this->router->setParam("addparam", "test");
-        return $this;
+    public function gameAttendance()
+    {
+
+        if (!$this->isLoggedIn) {
+            $this->setRedirectToSelf();
+            return $this->setNextRoute('/login');
+        } else {
+
+            // schedule
+            $s = new Schedule();
+            $this->rc->schedule = $s->getSchedule();
+
+            // game
+            $__schedule = $this->rc->schedule;
+            $__uid = $this->router->getParam('uid');
+            $__id = $this->cookies['id'];
+            $this->rc->game = Schedule::getGameFromSchedule($__schedule, $__uid);
+
+            // attendance
+            $query = "select * from attendance where uid = '$__uid' and rosterId = '$__id'";
+            $this->rc->userAttendance = $this->mysql->setQuery($query)->runRead();
+
+            // team attendance
+            $m = new MySQLHelper();
+            $this->rc->teamAttendance = $m->getRosterAttendanceByGameUid($__uid);
+
+            // who is going
+            $this->rc->attendance = $m->getRosterAttendanceThatIsGoingByGameUid($__uid);
+            // who is starting
+            $lineup = $m->getLineupByUid($__uid);
+            // if no starters yet, create it
+            if( count($lineup) === 0 ){
+                $m->createLineup($__uid);
+                $lineup = $m->getLineupByUid($__uid);
+            }
+            $this->rc->lineup = $lineup;
+
+            $this->view = 'schedule/attendance';
+            return $this;
+        }
     }
 
-    public function getScheduleById(){
-        $this->router->setParam("addparam", "test");
-        $this->router->setParam("thisrouter", true);
-        $this->view = 'schedule/id';
-        return $this;
+    public function updateGameAttendance()
+    {
+
+        // force a json response
+        $this->router->setVar('isAjax', 1);
+
+        if (!$this->isLoggedIn) {
+            return $this->setNextRoute('/login');
+        } else {
+
+            $__uid = $this->router->getVar('uid');
+            $__rosterId = $this->cookies['id'];
+            $__isGoing = $this->router->getVar('isGoing');
+
+            $s = new Schedule();
+            $result = $s->updateUserAttendance($__rosterId, $__uid, $__isGoing);
+            $this->rc->success = $result;
+            $this->rc->message = $result === true ? 'Game attendance successfully updated.' : $s->mysqlHelper->getError();
+
+            $m = new MySQLHelper();
+            $user = $m->getRosterById($__rosterId);
+
+            $schedule = $s->getSchedule();
+            $game = $s->getGameFromSchedule($schedule, $__uid);
+
+            $m = new MailManager();
+            $m->sendAttendanceUpdate($game, $user[0], $__isGoing);
+
+            return $this;
+        }
+
     }
 
-    public function test1(){
-        $this->view = 'schedule/test1';
-        return $this;
-    }
+    // helpers
 
-    public function test2(){
-        $this->view = 'schedule/test2';
-        return $this;
-    }
+
+
+
+
+
+
 
 }
 
-//WEEK 1
-//Sun 3/4	9:00 AM	Spirits (SR)	Tyros	Central Park 4
 
-//WEEK 2
-//Sun 3/11	9:00 AM	Tyros	Clockwork Orange	Sycamore 3
-
-//WEEK 3
-//Sun 3/18	9:00 AM	Inferno	Tyros	Sycamore 3
-
-//WEEK 4
-//Sun 3/25	11:00 AM	Renegades (SR)	Tyros	Central Park 4
-
-//WEEK 5
-//EASTER
-
-//WEEK 6
-//Sun 4/8	11:00 AM	FC Rehab	Tyros	Sycamore 3
-
-//WEEK 7
-//Sun 4/15	11:00 AM	Tyros	Inferno	Osage 3
-
-//WEEK 8
-//Sun 4/22	9:00 AM	Spirits (SR)	Tyros	Tiffany Roberts
-
-//WEEK 9
-//Sun 4/29	9:00 AM	Tyros	Tiburones (SR)	Sycamore 3
-
-//WEEK 10
-//Sun 5/6	9:00 AM	Sidekicks	Tyros	Osage 3
-
-//WEEK 11
-//MOTHER'S DAY
-
-//WEEK 12
-//Sun 5/20	9:00 AM	Renegades (SR)	Tyros	Central Park 4
-
-//WEEK 13
-//MEMORIAL DAY WEEKEND
-
-//WEEK 14
-//Sun 6/3	9:00 AM	Clockwork Orange	Tyros	Osage 3
-
-//WEEK 15
-//Sun 6/10	11:00 AM	Tyros	Sidekicks	Osage 3
