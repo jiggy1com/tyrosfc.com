827a6948e2e51ca1f8f4e15bfbf5d0e07aea178e
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-14 16:17:20 -0700

    adding more stuff

--- view/home/index.php
+++ view/home/index.php
@@ -1,16 +1,89 @@
 
 
-<div class="container-fluid">
+<div class="container-fluid mb-5">
     <div class="row">
         <div class="col-12">
             <div class="card">
                 <h1 class="card-header">
-                    Welcome to the Tyros website.
+                    Welcome to the Tyros website
                 </h1>
                 <div class="card-body">
-                    This is the home page.
+
+                    <p>Jersey Color: White</p>
+                    <p>Shorts Color: Black</p>
+                    <p>Socks Color: Optional</p>
+
+                    <p>I may be posting a "Snack List", so we know who is responsible for half-time oranges and beer after the game. Most likely, it'll have Florin's name for each week. =)</p>
+
+                    <img src="/img/florin.png" class="d-block mt-3 mr-auto ml-auto" />
+                    <h4 class="text-center">
+                        SnackMaster
+                    </h4>
+
+                    <p>Looking for the official league website? </p>
+
+                    <a href="http://www.diablosoccer.org/sites/dasl/schedule/183380/Spring-DASL-Division-C" target="_blank" class="btn btn-primary btn-block">
+                        Official League Site
+                    </a>
+
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="container-fluid mb-5">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <h1 class="card-header">
+                    Known Issues
+                </h1>
+                <div class="card-body">
+
+                    <ul>
+                        <li><span class="text-success">Resolved</span> Time stamps on the schedule page are wrong.</li>
+                        <li><span class="text-success">Resolved</span> No way to update password.</li>
+                        <li><span class="text-success">Resolved</span> Redirect after login is not correct.</li>
+                        <li><span class="text-success">Resolved</span> New team members do not have an account to log into the website.</li>
+                        <li><span class="text-success">Resolved</span> Attending is checked when the user has not yet made a selection (dammit).</li>
+                        <li><span class="text-success">Resolved</span> Login and Forgot Password should not be accessible if you're logged in.</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid mb-5">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <h1 class="card-header">
+                    Proposed Features
+                </h1>
+                <div class="card-body">
+
+                    <p>In no particular order.</p>
+
+                    <ul>
+                        <li>Add <strong>Artificial</strong> or <strong>Grass</strong> next to the location so we know which shoes to bring.</li>
+                        <li>Add a map of the field layout, with numbers so it's easier to find once at the location</li>
+                        <li>Add a profile pic =) so we can learn each other's name to face</li>
+                        <li>PayPal and/or Venmo integration to allow players to send funds to Jared, our treasurer.</li>
+                        <li>Create a bulk email tool to send automated and on-demand notifications.</li>
+                        <li>Integrate an SMS service to send automated and on-demand notifications.</li>
+                        <li>Add a checkbox for Remember Me & keep user logged in ... forever?</li>
+                        <li>Let players, not just admins, see who is and is not attending each game</li>
+                        <li>Track dues, for Jared</li>
+                        <li>Create some type of message board or notification system to let players contact the team w/o having to know everyone's email/#</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
