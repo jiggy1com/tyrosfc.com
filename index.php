f81c2d99c3e31e180b4179a9bbfd112a2d46a837
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-14 16:15:55 -0700

    show errors, add session

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
+<?php
+error_reporting(E_ALL);
+date_default_timezone_set('US/Pacific');
+ini_set('display_errors', 'On');
+
+// start session for session vars (logged in state, etc)
+session_start();
+?>
 <?php include('./Application.php') ?>
 <?php include('./view/layout/' . $GLOBALS['app']->layout . '.php'); ?>
