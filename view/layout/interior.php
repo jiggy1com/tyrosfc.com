4ab737c52bd46e1ddbd2e26e13afa31d880b7181
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-14 16:18:29 -0700

    force interior to be no more than 1140px

--- view/layout/interior.php
+++ view/layout/interior.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container-fluid" style="max-width: 1140px;">
     <div class="row">
         <div class="col-12 pt-5 pb-5 mb-5">
             <?php include('view/' . $app->view) . '.php' ?>
