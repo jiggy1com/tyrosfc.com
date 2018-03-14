762d11f6bceb32ccfce72ca7dbf5909a19158e79
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-14 16:17:47 -0700

    adding cachebuster for mweb caching issues during pushes, hide debug if not the correct IP

--- view/layout/default.php
+++ view/layout/default.php
@@ -1,3 +1,7 @@
+<?php
+$__LAYOUT_DATE = new DateTime();
+$__CACHEBUSTER = $__LAYOUT_DATE->format('mdYhm');
+?>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -5,6 +9,7 @@
     <title>TyrosFC.com</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/themes/lux.css" />
+    <link rel="stylesheet" href="/css/dev/app.css?<?= $__CACHEBUSTER ?>" />
     <!--
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     -->
@@ -12,13 +17,20 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="/js/app.js"></script>
+    <script src="/js/app.js?<?= $__CACHEBUSTER ?>"></script>
+
+
+
+    <script src="/js/Lineup.js?cachebuster=<?= $__CACHEBUSTER ?>"></script>
+    <script src="/js/Game.js?<?= $__CACHEBUSTER ?>"></script>
 </head>
 <body>
 
 <?php include('./view/layout/header.php'); ?>
 <?php include('./view/layout/interior.php'); ?>
 <?php include('./view/layout/footer.php'); ?>
+
+<?php if($_SERVER['REMOTE_ADDR'] === '216.239.124.44' || $_SERVER['REMOTE_ADDR'] === '127.0.0.1'):?>
 
 <div class="container-fluid d-none d-lg-block d-md-block d-xl-block">
     <div class="row">
@@ -33,6 +45,8 @@
         </div>
     </div>
 </div>
+
+<?php endif; ?>
 
 </body>
 </html>
