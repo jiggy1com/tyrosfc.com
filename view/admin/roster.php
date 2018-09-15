914bb3b2206ef942bac41ab7ff46ed4dfd9ed8c7
Joe Velez <joe.velez@cbsinteractive.com>
2018-09-14 22:18:02 -0700

    refactor, move template to partial. display active and inactive roster, partial now has form to update active status

--- view/admin/roster.php
+++ view/admin/roster.php
@@ -1,9 +1,8 @@
-<?php $__roster = $GLOBALS['app']->rc->roster; ?>
+<?php
+$__activeRoster = $GLOBALS['app']->rc->activeRoster;
+$__inActiveRoster = $GLOBALS['app']->rc->inActiveRoster;
 
-
-<script>
-
-</script>
+?>
 
 <div class="container-fluid">
     <div class="row">
@@ -13,35 +12,17 @@
                     Admin > Roster
                 </h1>
                 <div class="card-body">
-
                     <div class="row">
-                        <?php foreach($__roster as $key => $obj): ?>
-                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center mb-5">
 
-                                <h4><?= $obj->firstname ?></h4>
-                                <h4><?= $obj->lastname ?></h4>
+                        <?php $__roster = $__activeRoster; ?>
+                        <?php include('view/partial/roster.php') ?>
 
-                                <div class="row">
-                                    <div class="col-12 col-md-6 mb-3">
-                                        <a href="/admin/roster/sms/<?= $obj->id ?>" class="btn btn-primary btn-block">
-                                            SMS
-                                        </a>
-                                    </div>
-                                    <div class="col-12 col-md-6">
-                                        <a href="/admin/roster/email/<?= $obj->id ?>" class="btn btn-primary btn-block">
-                                            Email
-                                        </a>
-                                    </div>
-                                </div>
+                        <?php $__roster = $__inActiveRoster ?>
+                        <?php include('view/partial/roster.php') ?>
 
-                            </div>
-                        <?php endforeach; ?>
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
 </div>
-
